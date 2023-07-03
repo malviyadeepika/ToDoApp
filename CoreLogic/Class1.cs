066c996ef89f7372d974643dcdc9fb212ed892c4
Abuzar Nasim <abuzarnasim890@gmail.com>
2023-07-03 14:18:45 +0530

    Final Commit

--- CoreLogic/Class1.cs
+++ CoreLogic/Class1.cs
@@ -4,5 +4,10 @@
     {
         //Testing My Project   
       //Testing My Project
+      static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+      
     }
 }
