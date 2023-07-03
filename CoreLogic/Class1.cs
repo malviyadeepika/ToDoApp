519b2cd1d13d3088bf65ca751eb8c4f30058f2c3
Abuzar Nasim <abuzarnasim890@gmail.com>
2023-07-03 14:34:03 +0530

    good

--- CoreLogic/Class1.cs
+++ CoreLogic/Class1.cs
@@ -3,11 +3,6 @@
     public class Class1
     {
         //Testing My Project   
-      //Testing My Project
-      static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
-      
+          
     }
 }
